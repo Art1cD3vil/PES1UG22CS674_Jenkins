d1ed39911c55e3e9dd8a9287829d6a5bae36cbb1
Varad <144313199+Art1cD3vil@users.noreply.github.com>
2025-03-19 17:37:40 +0530

    Create PES1UG22CS674.cpp

---
+++ PES1UG22CS674.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Varad Vasudev Pai\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Hello, PES1UG22CS674\n";
+  
+  
+  return 0;
+}
