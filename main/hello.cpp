57a2b78cc060c941d8773cf33c356a7b8540c6e0
Art1cD3vil <varadpai27@gmail.com>
2025-03-19 16:17:29 +0530

    third commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, Varad Vasudev Pai\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  
   return 0;
 }
